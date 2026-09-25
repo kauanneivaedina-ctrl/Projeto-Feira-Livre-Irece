abbc2d21862a0f92ba52865bbd185ecc2c17f4f9
Kauan Neiva <kauanneivaedina@gmail.com>
2026-09-25 15:18:45 -0300

    organização de pastas e estrutura inicial do main (menu + switch)

--- src/main.c
+++ src/main.c
@@ -1,48 +1,112 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <windows.h>
+#include <stdlib.h>
 #include "includes/feirante.h"
-#include "includes/produto.h"
 
-int main()
-{
-    SetConsoleOutputCP(65001);
+void exibirMenu() {
+    printf("=========================================\n");
+    printf("\n=== SISTEMA DA FEIRA LIVRE DE IRECE ===\n");
+    printf("===========Escolha uma opcao:============\n");
+    printf("------------ Gerenciamento --------------\n");
+    printf("1. Cadastrar feirante\n");
+    printf("2. Listar todos os feirantes\n");
+    printf("3. Buscar feirante por codigo\n");
+    printf("4. Adicionar produto a banca\n");
+    printf("5. Remover feirante\n");
+    printf("\n");
+    printf("---------- Operacoes e Vendas ----------\n");
+    printf("6. Ordem de venda \n");
+    printf("7. Atualizar dia de feira\n");
+    printf("8. Remanejar feirante (banca e dia)\n");
+    printf("\n");
+    printf("-------- Financeiro e Relatorios --------\n");
+    printf("9. Calcular faturamento de um feirante\n");
+    printf("10. Calcular taxa da feira\n");
+    printf("11. Contar feirantes por dia\n");
+    printf("\n");
+    printf("------------ Arquivo e Saida ------------\n");
+    printf("12. Salvar dados em arquivo\n");
+    printf("13. Carregar dados do arquivo\n");
+    printf("0. Sair\n");
+}
 
-    Feirante feirantes[1];
-    int quantidade = 0;
-    int opcao_menu;
+int main() {
+    Feirante *feirantes = NULL;     //variaveis de controle do vetor dinamico
+    int quantidade = 0;             //
 
-    do
-    {
-        system("cls");
-        printf("===MENU DE OPÇÕES===\n\n");
-        printf("[1] Cadastrar feirante\n");
-        printf("[2] Listar feirantes\n");
-        printf("[3] Buscar feirante por código\n");
-        printf("[4] Atualizar dia frequentado pelo feirante\n");
-        printf("[5] Remover feirante\n");
-        printf("[6] Salvar alterações\n");
-        printf("[7] Carregar arquivo salvo\n");
-        printf("[8] Reiniciar número de feirantes\n");
-        printf("[9] Registrar venda\n");
-        printf("[10] Adicionar produto na banca\n");
-        printf("[11] Remanejar feirante\n");
-        printf("[12] Calcular faturamento de feirante\n");
-        printf("[13] Calcular taxa da feira\n");
-        printf("[14] Contar feirantes presentes no dia\n");
-        printf("[0] Sair do sistema\n");
-        printf("==================================================\n\n");
+    int opcao;
 
-        printf("Digite a opção desejada: ");
-        scanf("%d", &opcao_menu);
-
-        switch (opcao_menu)
-        {
-            
-            case 0: printf("Saindo do sistema...\n"); break;
-            default: printf("Opção inválida. Tente novamente.\n");
+    do {
+        exibirMenu();
+        if (scanf("%d", &opcao) != 1) {
+            printf("Entrada invalida! Digite um numero.\n");
+            while (getchar() != '\n');
+            continue;
         }
-    }while(opcao_menu != 0);
+
+        switch (opcao) {
+            case 1:
+                //cadastrar feirante no vtor
+                break;
+
+            case 2:
+                //listar todos
+                break;
+
+            case 3:
+                //buscar por codigo
+                break;
+
+            case 4:
+                //adicionar produto a banca de um feirante
+                break;
+
+            case 5:
+                //remover feirante
+                break;
+
+            case 6:
+                //ordem de venda
+                break;
+
+            case 7:
+                //atualizar dia de feira
+                break;
+
+            case 8:
+                //remanejar feirante
+                break;
+
+            case 9:
+                //calcular faturamento do feirante
+                break;
+
+            case 10:
+                //calcular taxa da feira
+                break;
+
+            case 11:
+                //contar feirantes por dia
+                break;
+
+            case 12:
+                //salvar em arquivo
+                break;
+
+            case 13:
+                //carregar de arquivo
+                break;
+
+            case 0:
+                printf("\nEncerrando o sistema...\n");
+                break;
+
+            default:
+                printf("\nOpcao invalida! Tente novamente.\n");
+        }
+
+    } while (opcao != 0);
+
+    liberarFeirantes(&feirantes, &quantidade);
 
     return 0;
 }
